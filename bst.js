7b0bea22edeceaa64544e879c105645528912e45
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2025-05-08 10:16:24 -0500

    Add node class

---
+++ bst.js
@@ -0,0 +1,13 @@
+import { mergesort } from "./odin-recursion/mergesort.js";
+
+class Node {
+  value = null;
+  left = null;
+  right = null;
+
+  constructor(value = null, left = null, right = null) {
+    this.value = value;
+    this.left = left;
+    this.right = right;
+  }
+}
